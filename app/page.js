725f9ecdec77f1063ad9f18c673648a3954986e1
Minsung Kim <tkfpsk@gmail.com>
2024-07-14 18:25:12 +0100

    fixed fetching

--- app/page.js
+++ app/page.js
@@ -2,18 +2,14 @@ import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from "@/components";
 import { getPosts } from '../Services';
 
-// const posts = [
-//   {title: 'react testing', excerpt: 'awkdnlkaw'},
-//   {title: 'hurr durr', excerpt: 'lkol ol ol'},
-// ]
-
-// export default async function Home( { posts } ) {
 const Home = async () => {
-  const posts = await getPosts();
-  // posts.forEach((post)=>){
-  //   console.log(posts.node.author);
-  // };
-  
+  let posts = [];
+  try {
+    posts = await getPosts();
+    console.log(posts)
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
 
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -21,28 +17,20 @@ const Home = async () => {
         <title>Minsung Kim</title>
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-          <div className="lg:col-span-8 col-span-1">
-            {posts.map((post, index) => (
-              <PostCard key={index} post={post.node} />
-            ))}
-            
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
-          <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relative top-8">
-              <PostWidget />
-              <Categories />
-            </div>
-          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
-// Fetch data at build time
-// export async function getStaticProps() {
-//   const posts = (await getPosts()) || [];
-//   return {
-//     props: { posts },
-//   };
-// }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eu-west-2.graphassets.com',
+        port: '',
+        // pathname: '//**',
+      }
+    ]
+  },
+};
 
 export default nextConfig;
